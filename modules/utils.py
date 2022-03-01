e5eb61e68f2b883284fdd920b5bf0963464b52ec
longdo12 <gialong12.12.99@gmail.com>
2022-03-01 17:31:46 +0700

    Preprocess data

--- modules/utils.py
+++ modules/utils.py
@@ -26,3 +26,31 @@ def get_tqdm():
         except:
             from tqdm import tqdm
             return tqdm
+
+class JsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, bson.ObjectId):
+            return str(obj)
+        else:
+            return super(JsonEncoder, self).default(obj)
+            
+def read_config(config):
+    if isinstance(config, str):
+        with open(config, "r", encoding="utf-8") as f:
+            config = json.load(f)
+    return config
+
+
+def save_config(config, path):
+    with open(path, "w") as file:
+        json.dump(config, file, cls=JsonEncoder)
+
+
+def if_none(origin, other):
+    return other if origin is None else origin
